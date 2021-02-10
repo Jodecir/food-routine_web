e14d01fcfbbbb70081422fe55c446b70688badeb
Douglas Oliveira <56686731+Jeffbeu@users.noreply.github.com>
2021-02-09 22:50:37 -0300

    Sobre o projeto
    Estou upando todo o template que ia me basear para construir esse projeto bastando fazer alterações no Código para fica do jeito que nós quisermos assim fica mais fácil trabalha em nosso projeto e todos podem mecher

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,28 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAgfYOA6fmUjVJT8vBqpNCZASpGEC2AU4M",
+    authDomain: "foodiesapp-3ff7f.firebaseapp.com",
+    projectId: "foodiesapp-3ff7f",
+    storageBucket: "foodiesapp-3ff7f.appspot.com",
+    messagingSenderId: "1008988281384",
+    appId: "1:1008988281384:web:622a6077d3b6a9434df31d",
+    measurementId: "G-6J1687GHCC"
+  },
+  onesignal: {
+    appId: '122e6b8b-b2d1-47e5-8cbd-434e510a0714',
+    googleProjectNumber: '',
+    restKey: 'NGNiZDVhODAtN2VkZi00NDNiLThhMmMtNTFhMGJmOTFlMDdj'
+  },
+  stripe: {
+    sk: ''
+  },
+  paypal: {
+    sandbox: '',
+    production: 'YOUR_PRODUCTION_CLIENT_ID'
+  },
+  general: {
+    symbol: '$',
+    code: 'USD'
+  }
 };
