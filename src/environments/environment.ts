f4e60f40332c1e16468f7a0d620154a632b4596d
Douglas Oliveira <56686731+Jeffbeu@users.noreply.github.com>
2021-02-10 09:36:25 -0300

    tradução
    começando a traduzir o projeto

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,7 @@
 
 export const environment = {
   production: false,
+  //firebase api
   firebase: {
   apiKey: "AIzaSyAgfYOA6fmUjVJT8vBqpNCZASpGEC2AU4M",
   authDomain: "foodiesapp-3ff7f.firebaseapp.com",
@@ -13,6 +14,7 @@ export const environment = {
   appId: "1:1008988281384:web:622a6077d3b6a9434df31d",
   measurementId: "G-6J1687GHCC"
   },
+  
   onesignal: {
     appId: '122e6b8b-b2d1-47e5-8cbd-434e510a0714',
     googleProjectNumber: '',
